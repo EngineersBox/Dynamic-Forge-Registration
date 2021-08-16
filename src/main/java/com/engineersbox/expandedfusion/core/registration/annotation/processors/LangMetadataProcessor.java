733b14006a43b9991e9b8f55b91ab5efe98e11c1
EngineersBox <toastcraft.info@gmail.com>
2021-08-17 01:12:28 +1000

    Updated todo for @LangMetadata processor with more details for supplying the Mod ID via JVM arguments/system properties

--- src/main/java/com/engineersbox/expandedfusion/core/registration/annotation/processors/LangMetadataProcessor.java
+++ src/main/java/com/engineersbox/expandedfusion/core/registration/annotation/processors/LangMetadataProcessor.java
@@ -6,7 +6,15 @@ public class LangMetadataProcessor {
      * TODO: Implement a class to generate the en_us.json file
      * it will need to handle checking if one exists already
      * and amending it with write-back. Otherwise creating a
-     * new one and filling it with relevant entries
+     * new one and filling it with relevant entries.
+     *
+     * When creating entries it should be of the form:
+     * "<TYPE>.<MOD ID>.<PROVIDER NAME>": "<NAME MAPPING>"
+     *
+     * The mod ID can be retrieved with System.getProperty("lang.metadata.mod.ID")
+     * where the Mod ID is provided as JVM args: -Xlang.metadata.mod.ID
      */
+
+    public static final String MOD_ID_SYSTEM_PROPERTY = "lang.metadata.mod.ID";
 
 }
